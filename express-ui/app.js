cc5197d2218f19b9b7a64deaf960b4eae44100c0
pgoldtho <peter@goldthorp.com>
2020-12-26 17:09:36 -0500

    remove express-handlebars

--- express-ui/app.js
+++ express-ui/app.js
@@ -1,56 +1,15 @@
-var createError = require('http-errors');
 var express = require('express');
-var exphbs  = require('express-handlebars');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
 var ora2pgRouter = require('./routes/ora2pg');
 
 var app = express();
 
-app.engine('hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs',
-  helpers: {
-    ifEquals: function (arg1, arg2, options) {
-        return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-      },
-    unlessEquals: function (arg1, arg2, options) {
-      return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
-    },
-    greaterThan: function(arg1, arg2, options) {
-      return (arg1 > arg2) ? options.fn(this): options.inverse(this);
-    },
-    assign: function (varName, varValue, options) {
-      if (!options.data.root) {
-        options.data.root = {};
-      }
-      options.data.root[varName] = varValue;
-    }
-  }
-}));
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/ora2pg', ora2pgRouter);
 app.use('/vue', express.static(path.join(__dirname, 'node_modules/vue/dist')));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 // error handler
 app.use(function(err, req, res, next) {
