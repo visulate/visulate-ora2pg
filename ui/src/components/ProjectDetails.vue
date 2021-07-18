b7365096f9cf49e41f34ec52df48ca424697e5b9
Simon Goldthorp <simon@goldthorp.com>
2021-07-18 12:45:50 -0400

    file list with download

--- ui/src/components/ProjectDetails.vue
+++ ui/src/components/ProjectDetails.vue
@@ -17,17 +17,26 @@
         </button>
       </span>
     </div>
-    <ul class="mdl-list">
-      <li v-for="file in fileList" class="mdl-list__item" :key="file">
-        <a :href="`${api_base}/ora2pg/project/${project}/download/${file}?key=${endpoints_key}`" class="link">{{
-          file
-        }}</a>
-      </li>
+    <ul class="mdl-list" style="padding-top: 0; padding-bottom: 0">
+      <li v-for="file in fileList" class="mdl-list__item" :key="file"
+        style="border-bottom: 1px solid #ddd">
+      
+        <span>{{file}}</span>
+        <span class="material-icons" @click="downloadFile(file)"
+         style="margin-left: auto; cursor: pointer" >file_download</span>
+      </li>  
     </ul>
-    <p style="padding-left: 10px">{{ fileCount }} files</p>
+    <div class="mdl-list__item" style="border-top: 1px solid #ddd">
+      <b>{{ fileCount }} files</b>
+      <div style="margin-left: auto"
+        @click="downloadFile(file)"><span class="material-icons">download_for_offline</span>
+        </div>
+    </div>
   </div>
+  
 </template>
 <script>
+import { httpGet } from "../assets/httpClient";
 export default {
   name: 'ProjectDetails',
   data() {
@@ -51,6 +60,18 @@ export default {
     },
     closeComponent() {
       this.$emit('close-component');
+    },
+    async downloadFile(file) {
+      const url = `/ora2pg/project/${this.project}/download/${file}`;
+      const res = await httpGet(url);
+
+      const blob = await res.blob();
+      const localfile = window.URL.createObjectURL(blob)
+
+      let link = document.createElement('a');
+      link.href = localfile;
+      link.download = file;
+      link.click();
     }
   },
   computed: {
