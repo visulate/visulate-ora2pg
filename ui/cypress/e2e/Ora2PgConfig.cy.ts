bc1d072313e82ca64186c22fb04eec68c3155e0c
sydney <sydneygoldthorp@gmail.com>
2022-07-12 12:26:25 -0400

    add comments

--- ui/cypress/e2e/Ora2PgConfig.cy.ts
+++ ui/cypress/e2e/Ora2PgConfig.cy.ts
@@ -9,16 +9,21 @@ describe('Ora2Pg Config Tests', () => {
       // GIVEN
       cy.get('[data-cy="run_page"]').should('not.exist');
     
+      // Enter default project
       cy.get('.mdl-layout__drawer-button').click();
       cy.get('[data-cy="project_list"]').children().first().click();
       cy.get('.mdl-layout__obfuscator').click();
 
+      // Enable dsn
       cy.get('[data-cy="dsn_enabled_ORACLE_DSN"]').check();
 
+      // Enter dsn
       cy.get('[data-cy="dsn_ORACLE_DSN"]').clear();
       cy.get('[data-cy="dsn_ORACLE_DSN"]').type('dbi:Oracle:host=testdb;sid=XE;port=1521');
+      // Click away to make the auth dialog appear
       cy.get('body').click();
 
+      // Enter credentials
       cy.get('[data-cy="auth_oracle_user"]').type('system');
       cy.get('[data-cy="auth_oracle_pwd"]').type('oracle');
       cy.get('[data-cy="submit_auth"]').click();
@@ -37,8 +42,7 @@ describe('Ora2Pg Config Tests', () => {
         expect(interception.response.body).to.contain('ora2pg complete');
         expect(interception.response.body).to.contain('Removing config file');
         expect(interception.response.body).to.contain('Creating compressed file default.tar.gz');
-
-      })
+      });
 
       // CLEANUP
       sessionStorage.setItem('dbi:Oracle:host=testdb;sid=XE;port=1521', '');
